dad85469ee41da94986fd0a13390f15e0aa234ba
rsgrey <rserega@outlook.com>
2023-08-10 23:09:58 +0600

    Add bootstrap5

--- src/CrudAsset.php
+++ src/CrudAsset.php
@@ -30,10 +30,23 @@ class CrudAsset extends AssetBundle
     public function init()
     {
         $isBs4 = StringHelper::startsWith(ArrayHelper::getValue(\Yii::$app->params, 'bsVersion', ''), '4.');
+        $isBs5 = StringHelper::startsWith(ArrayHelper::getValue(\Yii::$app->params, 'bsVersion', ''), '5.');
 
         array_push($this->depends, 'yii\web\YiiAsset');
-        array_push($this->depends, $isBs4 ? 'yii\bootstrap4\BootstrapAsset' : 'yii\bootstrap\BootstrapAsset');
-        array_push($this->depends, $isBs4 ? 'yii\bootstrap4\BootstrapPluginAsset' : 'yii\bootstrap\BootstrapPluginAsset');
+        if ($isBs5){
+            array_push($this->depends, 'yii\bootstrap5\BootstrapAsset');
+            array_push($this->depends, 'yii\bootstrap5\BootstrapPluginAsset');
+        }
+        else{
+            if ($isBs5){
+                array_push($this->depends, 'yii\bootstrap4\BootstrapAsset');
+                array_push($this->depends, 'yii\bootstrap4\BootstrapPluginAsset');
+            }
+            else{
+                array_push($this->depends, 'yii\bootstrap\BootstrapAsset');
+                array_push($this->depends, 'yii\bootstrap\BootstrapPluginAsset');
+            }
+        }
         array_push($this->depends, 'kartik\grid\GridViewAsset');
         array_push($this->depends, 'ivankff\yii2ModalAjax\JqueryFormAsset');
     }
